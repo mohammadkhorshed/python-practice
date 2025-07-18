0d42cfe9d4b1b30cfa4bee40a2f0378dec0050e6
grayMafia <khorshed1921@gmail.com>
2025-07-18 23:29:22 +0600

    Add basic structure

---
+++ oop/book_library.py
@@ -0,0 +1,21 @@
+class Library:
+    def __init__(self):
+        pass
+
+    def add_book(self):
+        pass
+
+    def borrow_book(self):
+        pass
+
+    def return_book(self):
+        pass
+
+    def display_available_books(self):
+        pass
+
+class Book:
+    def __init__(self):
+        self.title = ""
+        self.author = ""
+        self.is_available = True
